008bdfa7df4e06bee62a31631321a19512c8edf9
Gajanan Deshmukh <159412495+Gajanand1219@users.noreply.github.com>
2026-09-08 15:52:16 +0530

    Update main.py

--- main.py
+++ main.py
@@ -73,7 +73,7 @@ app = FastAPI(
 app.add_middleware(
     CORSMiddleware,
     allow_origins=[
-        "https://shivyogelectronics.vercel.app/",
+        "https://shivyogelectronics.vercel.app",
         "http://localhost:5173",
     ],
     allow_credentials=True,
