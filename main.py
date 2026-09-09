f28b36b7bdc42ecff1f6c747ba26a116e023ceee
Gajanan Deshmukh <159412495+Gajanand1219@users.noreply.github.com>
2026-09-09 17:45:26 +0530

    Refactor FastAPI application structure
    Refactor FastAPI application to improve structure and readability.

--- main.py
+++ main.py
@@ -73,8 +73,7 @@ app = FastAPI(
 app.add_middleware(
     CORSMiddleware,
     allow_origins=[
-        "https://shivyogelectronics.vercel.app",
-        "https://test-kappa-indol-32.vercel.app",
+        "https://shivyogelectrical.vercel.app",
         "http://localhost:5173",
     ],
     allow_credentials=True,
